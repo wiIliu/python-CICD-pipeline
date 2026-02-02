887196fb260c95fcd7deece533e4900468dff121
wiIliu <willowconnelly@oakland.edu>
2026-02-02 01:16:09 -0500

    generate order schemas

--- orders_service/app/schemas/order.py
+++ orders_service/app/schemas/order.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel
 
-class Order(BaseModel):
+class OrderCreate(BaseModel):
     name: str
     product: str
     count: int
     price: float
+
+class OrderResponse(BaseModel):
+    id: int
+    name: str
+    product: str
+    count: int
+    price: float
+    class Config:
+        from_attributes = True
+        
