9aefee7287d75484f7f7920756acabcb019a0c0e
wiIliu <willowconnelly@oakland.edu>
2026-02-10 15:22:54 -0500

    resturcture - modified testing's db foundation and incorrporated alembic migrations automatically

--- tests/orders_service_tests/conftest.py
+++ tests/orders_service_tests/conftest.py
@@ -1,5 +1,13 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql+psycopg://test1:test1@localhost:5432/orders_TEST"
+)
+
 import pytest
-# from dotenv import load_dotenv
+from alembic import command
+from alembic.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi.testclient import TestClient
@@ -8,15 +16,23 @@ from ..factories.order_factory import OrderFactory
 from orders_service.app.core.config import settings
 from orders_service.app.dependencies.db import get_db
 
-# load_dotenv(".env.test",override=True)
 
+#TODO: path change - alembic.ini
+@pytest.fixture(scope="session", autouse=True)
+def migrate_db():
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
 
+    yield
+
+#TODO: put in function?
 engine = create_engine(settings.DATABASE_URL)
 TestingSession = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine,
 )
+
 
 @pytest.fixture
 def client(db: Session):
@@ -29,6 +45,7 @@ def client(db: Session):
         yield tc
     app.dependency_overrides.clear()
 
+
 @pytest.fixture(scope="function", autouse=True)
 def db():
     connection = engine.connect()
@@ -40,6 +57,7 @@ def db():
     session.close()
     transaction.rollback()
     connection.close()
+
 
 @pytest.fixture(autouse=True)
 def set_session_for_factories(db: Session):
