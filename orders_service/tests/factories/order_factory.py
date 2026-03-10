b1d7b801625df294ac189d321f4ade2abcbe7a0b
wiIliu <willowconnelly@oakland.edu>
2026-03-10 13:54:57 -0400

    Misc - removed unnecessary lines

--- orders_service/tests/factories/order_factory.py
+++ orders_service/tests/factories/order_factory.py
@@ -1,5 +1,3 @@
-import random
-from uuid import uuid4
 import factory
 from orders_service.app.models import order
 
@@ -8,7 +6,6 @@ class OrderFactory(factory.alchemy.SQLAlchemyModelFactory):
         model = order.Order
         sqlalchemy_session_persistence = "flush"
 
-    # id = factory.LazyFunction(uuid4)
     name = factory.Faker("name")
     product = factory.Sequence(lambda n: 'item %d' % n)
     count = factory.Faker("random_int", min=1, max=100)
